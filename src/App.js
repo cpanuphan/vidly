adbad958a7495b6dce1876fe6e07153b2d1de2cc
Panuphan@BananaCoding <panuphan@bananacoding.com>
2019-10-31 21:59:41 +0700

    Destructor component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import "./App.css";
 import Movies from "./components/movies";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <div>
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#top">
+          Navbar
+        </a>
+      </nav>
+
+      <main className="container">
+        <Route path="/movies" component={Movies} />
+        {/* <Movies /> */}
+      </main>
+    </div>
   );
 }
 
